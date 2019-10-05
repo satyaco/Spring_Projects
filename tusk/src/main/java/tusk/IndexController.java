be4f0fac9f69f666faf1c708ec086d921e7e2777
satyaco <amsatymail@gmail.com>
2019-10-05 10:17:31 +0530

    First Commit Uploading all the files

---
+++ tusk/src/main/java/tusk/IndexController.java
@@ -0,0 +1,13 @@
+package tusk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String home(){
+		return "index.jsp";
+	}
+}
---
+++ tusk/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>
+Welcome to My Spring Practice Project...!
+</h2>
+</body>
+</html>
